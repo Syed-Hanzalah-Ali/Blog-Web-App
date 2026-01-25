08d54a875bb60fc4f76a53d033ece12859a913c5
Syed-Hanzalah-Ali <syedhanzalahali1062@gmail.com>
2026-01-25 12:48:31 +0500

    forth commit- redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { authService } from "./appwrite/auth";
+import { login, logout } from "./store/auth.Slice.js";
+import { useDispatch } from "react-redux";
+import { Footer, Header } from "./components/index.js";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        // runs everytime
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      {loading === false ? (
+        <div className="w-full-block">
+          <Header />
+          <main></main>
+          <Footer />
+        </div>
+      ) : (
+        <div>loading</div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
